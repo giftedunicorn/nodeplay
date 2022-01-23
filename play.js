fdcdc6bdc24107277953bc67ffb83d35e8efb6c2
ednew2018 <shenjian8628@gmail.com>
2022-01-23 23:16:26 +0800

    Add more to play

--- play.js
+++ play.js
@@ -1,3 +1,4 @@
+
 // deep clone
 const deepClone = function(obj) {
 	if (!obj) return obj
@@ -43,9 +44,9 @@ function saveInput(id){
   console.log('Saving data', id);
 }
 const testDebounce = debounce((id) => saveInput(id));
-// setInterval(() => {
-// 	testDebounce(12)
-// }, 250)
+setInterval(() => {
+	testDebounce(12)
+}, 250)
 
 const throttle = function(fn, timeout = 500) {
 	let waiting = false
@@ -68,18 +69,117 @@ setInterval(() => {
 }, 250)
 
 // promise all
+const promiseAll = function(inputs) {
+	return new Promise((resolve, reject) => {
+		let length = inputs.length
+		let count = 0
+		let data = []
+		for (let fn of inputs) {
+			fn.apply(this)
+			.then((res) => {
+				count++
+				data.push(res)
+				if (count === length) resolve(data)
+			}).catch((err) => {
+				reject(err)
+			})
+		}
+	})
+}
+const request = function(id) {
+	return new Promise((resolve, reject) => {
+	   let timeout = Math.floor(Math.random() * 1000) + 500
+       setTimeout(() => {
+           resolve(`${id}: ${timeout}ms`);
+       }, timeout);
+	})	
+}
+let inputs = []
+for (let i = 0; i < 22; i++) {
+	inputs.push(() => request(i))
+}
+promiseAll(inputs)
+.then((res) => {
+	console.log('promiseAll', res)
+})
 
 // promise race
+const promiseRace = function(inputs) {
+	return new Promise((resolve, reject) => {
+		for (let fn of inputs) {
+			fn.apply(this)
+			.then((res) => {
+				resolve(res)
+			}).catch((err) => {
+				reject(err)
+			})
+		}
+	})
+}
+promiseRace(inputs)
+.then((res) => {
+	console.log('promiseRace', res)
+})
 
 // promise controll
+const scheduler = function(max = 3) {
+	let current = 0
+	let queue = []
+
+	function start() {
+		if (queue.length === 0 || current >= max) return
+
+		current++
+		let [fn, resolve, reject] = queue.shift()
+		fn.apply(this).then((res) => {
+			current--
+			start()
+			resolve(res)
+		}).catch((err) => {
+			reject(err)
+		})
+	}
+
+	return function(fn) {
+		return new Promise((resolve, reject) => {
+			queue.push([fn, resolve, reject])
+			start()
+		})
+	}
+}
+let helper = scheduler(6)
+for (let i = 0; i < 25; i++) {
+	helper(() => request(i))
+    .then((res) => {
+		console.log('scheduler', res)
+	})
+}
 
 // reduce
+Array.prototype.myReduce = function(fn, num) {
+	let nums = this
+	let res = 0
+	if (num) res = num
 
+	for (let i = 0; i < nums.length; i++) {
+		res = fn(res, nums[i])
+	}
 
+	return res
+}
+let arr = [1,2,3,4,3,1]
+let myReduceRes = arr.myReduce((a, b) => {
+  return a + b
+})
+let reduceRes = arr.reduce((a, b) => {
+  return a + b
+})
+console.log('myReduce', myReduceRes)
+console.log('reduce', reduceRes)
 
-
-// bind
 // new
+// bind
 // AJAX
 // myInstanceof
 // json to string
+
