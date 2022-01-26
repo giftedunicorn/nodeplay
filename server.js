ad11ba3223526eb1f170135a41a75785d07dbb60
ednew2018 <shenjian8628@gmail.com>
2022-01-26 21:26:30 +0800

    Try cache

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const http = require('http')
+const crypto = require('crypto');
+
+function updateTime() {
+    setInterval(() => this.time = new Date().toUTCString(), 1000)
+    return this.time
+}
 
 http.createServer((req, res) => {
     console.log('url:', `${req.method} ${req.url} `)
 
     const { url } = req
     if ('/' === url) {
+        // 在这里加入cache-control和expires无效，浏览器默认设置请求为 cache-controll: max-age=0
         res.end(`
             <html>
                 <!-- <meta http-equiv="Refresh" content="5" /> -->
@@ -14,7 +21,30 @@ http.createServer((req, res) => {
             `)
     } else if (url === '/main.js') {
         const content = `document.writeln('<br>JS   Update Time:${updateTime()}')`
-       
+
+        // cache-control优先级高于expires，最后会缓存20s。
+        // res.setHeader('Expires', new Date(Date.now() + 10 * 1000).toUTCString())
+        // res.setHeader('Cache-Control', 'max-age=20')
+
+        res.setHeader('Cache-Control', 'no-cache')
+        res.setHeader('last-modified', new Date().toUTCString())
+        if (new Date(req.headers['if-modified-since']).getTime() + 3 * 1000 > Date.now()) {
+            console.log('协商缓存命中....')
+            res.statusCode = 304 // not modified
+            res.end()
+            return
+        }
+
+        // res.setHeader('Cache-Control', 'no-cache')
+        // const hash = crypto.createHash('sha1').update(content).digest('hex')
+        // res.setHeader('Etag', hash)
+        // if (req.headers['if-none-match'] === hash){
+        //   console.log('Etag协商缓存命中.....')
+        //   res.statusCode = 304
+        //   res.end()
+        //   return 
+        // }
+
         res.statusCode = 200
         res.end(content)
     } else if (url === '/favicon.ico') {
